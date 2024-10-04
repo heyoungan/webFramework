196e0ae54cb068d29750eee8dc0332f309bf639d
heyoungan <159891097+heyoungan@users.noreply.github.com>
2024-10-04 19:25:36 +0900

    chapter

---
+++ 36_data_type.js
@@ -0,0 +1,26 @@
+// Number type
+let n1 = 1234;
+let n2 = 5.678;
+
+// String type
+let s1 ="hello";
+let s2 = 'world';
+
+// Boolean type
+let b1 = true;
+let b2 = false;
+
+// Null type
+let n = null; // 정의된 값이 null
+
+// Undefined type
+let u1;
+let u2 = undefined; // 미정의 된 상태. 향후 대입문에 의해 number나 string등 다양한 type이 됨
+
+// Array type
+let arr = [1,2,3,4];
+
+// Object type
+let obj = {a:"apple", b:"banana", c:"carror"};
+
+
